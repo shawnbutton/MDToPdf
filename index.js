0ef32de7685d8738c67f5ba92eea20d0d1b394f6
Shawn Button <Shawn.Button@scotiabank.com>
2017-06-28 19:18:25 -0400

    moved conversion to own file

--- index.js
+++ index.js
@@ -6,10 +6,13 @@
 //https://www.bountysource.com/issues/40330704-typeerror-bad-argument
 //https://github.com/Medium/phantomjs/issues/533
 
-var markdownpdf = require("markdown-pdf");
+
+var convert = require("./lib/convert");
+
+var inputName = "sample/roman_numeral_calculator.md";
+var outputName = "output.pdf";
+
+convert(inputName, outputName);
 
 
-markdownpdf().from("sample/roman_numeral_calculator.md").to("output.pdf", function () {
-    console.log("Done")
-});
 
