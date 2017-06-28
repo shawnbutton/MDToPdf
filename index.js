f1503a2c27857cfc39efef35087038a95011725d
Shawn Button <Shawn.Button@scotiabank.com>
2017-06-28 18:55:46 -0400

    added travis run index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+//Uses markdownpdf
+//https://www.npmjs.com/package/markdown-pdf
+
 // you MIGHT need to run the following first to install phantomjs (especially on mac?):
 // node node_modules/phantomjs-prebuilt/install.js
 //https://www.bountysource.com/issues/40330704-typeerror-bad-argument
@@ -5,10 +8,11 @@
 
 
 
+
 var markdownpdf = require("markdown-pdf");
 
 
-markdownpdf().from("/Users/shawnbutton/Documents/Programming/projects/training-java/katas/src/main/java/romannumeralcalculator/roman_numeral_calculator.md").to("md.pdf", function () {
+markdownpdf().from("/Users/shawnbutton/Documents/Programming/projects/training-java/katas/src/main/java/romannumeralcalculator/roman_numeral_calculator.md").to("output.pdf", function () {
     console.log("Done")
 });
 
