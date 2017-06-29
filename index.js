ea332467298ffcd0279a175ae172125cb4ea8f5d
Shawn Button <Shawn.Button@scotiabank.com>
2017-06-28 20:01:18 -0400

    cleaned up ignore files. Formatted comments.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 //Uses markdownpdf
 //https://www.npmjs.com/package/markdown-pdf
 
-// you MIGHT need to run the following first to install phantomjs (especially on mac?):
+// You MIGHT need to run the following first to install phantomjs (especially on mac?):
 // node node_modules/phantomjs-prebuilt/install.js
-//https://www.bountysource.com/issues/40330704-typeerror-bad-argument
-//https://github.com/Medium/phantomjs/issues/533
+// if so we can include this in the package.json for the future.
+// see:
+//   https://www.bountysource.com/issues/40330704-typeerror-bad-argument
+//   https://github.com/Medium/phantomjs/issues/533
 
 
 var convert = require("./convert");
